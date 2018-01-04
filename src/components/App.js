60de8c2a4b2c6cbb95c0304e736a06f87c42a092
Joseph Gordy <jgordy2424@gmail.com>
2018-01-04 17:10:29 -0500

    successfully posted a photo to the Google Vision API

---
+++ src/components/App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import vision from "react-cloud-vision-api";
+vision.init({ auth: 'AIzaSyAS_9EhaNTu1UtMPgKfNQt8-fOpe8DExOI'})
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageURL: ''
+    }
+  }
+
+  convertCanvasToImage = (canvas) => {
+    let image = new Image();
+
+    image.src = canvas.toDataURL("image/png");
+    let imgURL = image.src.replace("data:image/png;base64,", "")
+    // console.log(image.src);
+    this.setState({imageURL: imgURL});
+
+  }
+
+  snapPhoto = (width, height) => {
+    let canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        video = document.getElementById('video'),
+        imageWidth,
+        imageHeight;
+
+    if (width <= 850) {
+      imageWidth = width;
+      imageHeight = height;
+    } else {
+      imageWidth = width / 2;
+      imageHeight = height / 1.45;
+    }
+
+    context.drawImage(video, 0, 0, imageWidth, imageHeight);
+
+    this.convertCanvasToImage(canvas);
+  };
+
+  submitPhoto = () => {
+
+    const req = new vision.Request({
+      image: new vision.Image({
+        base64: this.state.imageURL,
+      }),
+      features: [
+        new vision.Feature('LABEL_DETECTION', 5),
+      ]
+    })
+
+    vision.annotate(req).then((res) => {
+      // handling response
+      console.log(res.responses[0])
+    }, (e) => {
+      console.log('Error: ', e)
+    })
+
+  };
+
+  componentDidMount() {
+
+    let video = document.getElementById('video'),
+        canvas = document.getElementById('canvas');
+
+    // camera access
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then(function(stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+          });
+    }
+
+    if(window.innerWidth <= 850) {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      video.width = window.innerWidth;
+      video.height = window.innerHeight;
+    } else {
+      video.width = window.innerWidth / 2;
+      video.height = 480;
+      canvas.width = video.width;
+      canvas.height = video.height;
+    }
+  };
+
+  render() {
+    // console.log(this.state.imageURL);
+    return (
+      <div className="App">
+
+        <video id="video" width='' height='' autoPlay></video>
+
+        <canvas id="canvas" width='' height=''></canvas>
+
+        <img src='' id='image' alt=""/>
+
+        <div id="button">
+          <button id="snap" onClick={() => this.snapPhoto(window.innerWidth, window.innerHeight)}>
+            <i className="material-icons">photo_camera</i>
+          </button>
+          <button id="submit" onClick={() => this.submitPhoto()}>
+            <i className="material-icons">send</i>
+          </button>
+        </div>
+
+        {/*<div>
+          <button id="submit" onClick={() => this.submitPhoto()}>
+            <i className="material-icons">send</i>
+          </button>
+        </div>*/}
+
+      </div>
+    );
+  }
+}
+
+export default App;
