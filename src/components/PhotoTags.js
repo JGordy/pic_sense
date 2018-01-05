23939c2659a0fde3acc29c76eb35b5cebb8e02ed
Joseph Gordy <jgordy2424@gmail.com>
2018-01-05 14:24:30 -0500

    cleaned up the display after labels come back. Added the confidence score to the screen and gave each button a different color depending on that confidence score.

--- src/components/PhotoTags.js
+++ src/components/PhotoTags.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 export default class PhotoTags extends Component {
 
   render() {
-    console.log("This.props", this.props);
     let tagList = this.props.tags.map((tag, index) => {
+      console.log(tag);
+      let score = tag.score * 100,
+      scoreStyle;
+
+      if (score >= 95) {
+        scoreStyle = {backgroundColor: 'rgba(70, 130, 180, 0.8)'}
+      }
+      else if (score >= 85) {
+        scoreStyle = {backgroundColor: "rgba(60, 179, 113, 0.8)"}
+      } else {
+        scoreStyle = {backgroundColor: "rgba(255, 99, 71, 0.7)"}
+      }
       return (
-        <div key={index} className="imageTags" onClick={() => this.props.speak(tag.description)}>
-          {tag.description}
+        <div key={index} style={scoreStyle} className="imageTags" onClick={() => this.props.speak(tag.description)}>
+          <h3>{tag.description}</h3><h5>{score.toFixed(0)}% sure</h5>
         </div>
       )
     })
